cee1c5e644eef752a2329b4eb73204ef32d59813
Muhammad Usman <usmanupgrade@gmail.com>
2026-03-04 16:17:44 +0500

    message history update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_ollama import ChatOllama
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 
 llm = ChatOllama(
@@ -10,12 +10,34 @@ llm = ChatOllama(
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful AI assistant"),
+    MessagesPlaceholder(variable_name="chat_history"),
     ("human", "{question}")
     ])
 
 chain = prompt | llm | StrOutputParser()
 
+chat_history = []
+
+def chat(question):
+    response = chain.invoke(
+        {
+            "question": question,
+            "chat_history": chat_history
+        }
+    )
+    
+    chat_history.append(AIMessage(content="response"))
+    chat_history.append(HumanMessage(content=question))
+    
+    return response
 
 
-for chunk in chain.stream({"question": "What is RAG?"}):
-    print(chunk, end="", flush=True)
+print(chat("What is Rag?"))
+print(chat("give me a Python example of it"))
+print(chat("Now explain the code you just gave"))
+
+
+
+
+#for chunk in chain.stream({"question": "What is RAG?"}):
+#    print(chunk, end="", flush=True)
