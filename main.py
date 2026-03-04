7d34e395bc777940458427b16135f551e7f109f4
Muhammad Usman <usmanupgrade@gmail.com>
2026-03-04 15:50:12 +0500

    NewUpdate

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 llm = ChatOllama(
     model ="glm-5:cloud",
     temprature=0.7
 )
-response = llm.invoke("What is Rag?")
-print(response.content)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI assistant"),
+    ("human", "{question}")
+    ])
+
+chain = prompt | llm | StrOutputParser()
+
+response = chain.invoke({"question": "What is RAG?"})
+print(response)
+
+for chunk in chain.stream({"question": "What is RAG?"}):
+    print(chunk, end="", flush=True)
