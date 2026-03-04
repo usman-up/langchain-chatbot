61e3b6d86046ce148bb8f32f720fa36a50251a32
Muhammad Usman <usmanupgrade@gmail.com>
2026-03-04 14:23:03 +0500

    :sparkles: add LangChain Ollama setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from lanchain-chatbot!")
-
-
-if __name__ == "__main__":
-    main()
+from langchain_ollama import ChatOllama
+llm = ChatOllama(
+    model ="glm-5:cloud",
+    temprature=0.7
+)
+response = llm.invoke("What is Rag?")
+print(response.content)
